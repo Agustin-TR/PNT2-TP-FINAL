bbc0961c97f028779447654f351a52f2511fbb73
adrian.addesso <adrian.addesso@gmail.com>
2025-11-02 20:50:53 -0300

    Downloaded login/register implementation and configured watchlist to use the new renamed services

--- src/components/Watchlist.vue
+++ src/components/Watchlist.vue
@@ -4,11 +4,9 @@
 </template>
 
 <script>
-import ServicioPeliculas from '@/servicios/peliculas';
+import movieService from "../services/movies";
 
-const ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NjczMjI2MGUyZGE5MGEzOGEwNzhkZDEwM2MyODRmZiIsIm5iZiI6MTc2MTgzNDI3Ni4yODEsInN1YiI6IjY5MDM3NTI0M2FjMWEzNWM4NmU5OTZkOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1PT2L_iOfHyAHqAgk56ERvoKF0ojmKDMwr2UOIqpI-w";
-
-const peliculasService = new ServicioPeliculas(ACCESS_TOKEN);
+const BASE_IMAGE_URL = import.meta.env.VITE_IMG_BASE_URL;
 
     export default {
         name: 'Watchlist', // 🔑 Cambiamos el nombre del componente
@@ -22,7 +20,7 @@ const peliculasService = new ServicioPeliculas(ACCESS_TOKEN);
             async getPeliculas(id){
                 id = 1218925 
                 // This is correct IF the service file uses 'export default { ... }'
-                this.pelicula = await ServicioPeliculas.getDetallesPelicula(id) 
+                this.pelicula = await MovieService.getDetallesPelicula(id) 
             }
             },
         mounted(){
